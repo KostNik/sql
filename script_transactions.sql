4e8fab8a2e16027e18320d5e422a874425b9520a
Kostiuk Mykyta <kost.nik88@gmail.com>
2019-04-13 20:02:06 +0300

    add northwind

--- script_transactions.sql
+++ script_transactions.sql
@@ -14,7 +14,8 @@ begin;
 select (@salary_ss := salary)
 from salaries
 where emp_no = 10009
-  and to_date = '9999-01-01' FOR UPDATE;
+  and to_date = '9999-01-01' FOR
+UPDATE; #make Select in transaction, thus it is waiting for other transaction ends
 
 UPDATE salaries
 set salary = (@salary_ss + 20)
