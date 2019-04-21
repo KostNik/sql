e3d99e0ffcd92da8827672f2b93acc387d4e00eb
Kostiuk Mykyta <kost.nik88@gmail.com>
2019-04-21 23:10:08 +0300

    procedure, params

--- basic/advance/stored_procedure.sql
+++ basic/advance/stored_procedure.sql
@@ -9,13 +9,14 @@ delimiter $$
 create procedure HelloWorld()
 begin
   select (@output := 'output');
-  select concat(@output,' -> ','Hello World!!!');
+  select concat(@output, ' -> ', 'Hello World!!!');
 end$$
 
 delimiter ;
 
 
-select cast(body AS CHAR) from mysql.proc;
+select cast(body AS CHAR)
+from mysql.proc;
 
 
 #Set DEFINER (permissions)
@@ -25,15 +26,31 @@ grant execute on procedure employees.HelloWorld to shopuser@localhost;
 
 delimiter $$
 
-create definer=shopuser@localhost procedure HelloWorld() #call as shopuser@localhost not as root
-sql security definer
+create
+  definer =shopuser@localhost procedure HelloWorld() #call as shopuser@localhost not as root
+  sql security definer
 begin
   select (@output := 'output');
-  select concat(@output,' -> ','Hello World!!!');
+  select concat(@output, ' -> ', 'Hello World!!!');
 end$$
 
 delimiter ;
 
 SELECT CURRENT_USER;
 
+# Set PARAMS
 
+delimiter $$
+
+create procedure ShowEmployees(in name varchar(255), in date DATE)
+begin
+  select *
+  from employees
+  where employees.employees.first_name like name
+    and employees.employees.birth_date >= date;
+end $$
+
+delimiter ;
+
+
+call ShowEmployees('%ill%', STR_TO_DATE('12/12/1964', '%m/%d/%Y'));
